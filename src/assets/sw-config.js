232d0ac8ed948d91affd441277bad15ca8dae9ae
Daniel Costa <daniel.cfdutra13@gmail.com>
2018-05-01 02:46:04 -0300

    Worker adicionado

---
+++ src/assets/sw-config.js
@@ -0,0 +1,24 @@
+var config = {
+  'assets': [
+    '/favicon.ico',
+    '/robots.txt',
+    '/index.html',
+    '/assets/app-icon.png',
+    '/assets/fontello.svg',
+    '/assets/styles.css',
+    '/assets/styles.css.map',
+    '/assets/styles.css',
+    '/assets/js/app.js',
+    '/assets/js/vendor.js',
+    '/offline-redirect/'
+  ],
+  'paths': {
+    'api': 'https://wptavern.com/wp-json/wp/v2',
+    'remote': 'https://wptavern.com/',
+    'client': 'https://steamtacao.netlify.com/'
+  },
+  'cacheNames': {
+    'assetCache': 'vwpCacheAsset',
+    'remoteCache': 'vwpCacheRemote'
+  }
+}
